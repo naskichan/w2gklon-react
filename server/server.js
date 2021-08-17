fda5e61f859a38ff1a22a2ec898183f6a5b8bfda
Naskichan <gitlab@naski.org>
2021-08-17 16:38:42 +0200

    playlists endpoint with test data

--- server/server.js
+++ server/server.js
@@ -3,9 +3,14 @@ const app = express()
 const server = require('http').createServer(app)
 
 const io = require('socket.io')(server)
+let db = require('./data/db.json')
 
 const port = 3001
 
 app.listen(port, function() {
     console.log('backend listening to', port)
+})
+
+app.get('/api/playlists', function(req, res) {
+    res.status(200).send(db.playlists)
 })
