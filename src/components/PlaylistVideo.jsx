f155534cbba4a2045e352a6090ea6e195cf20ec5
Naskichan <gitlab@naski.org>
2021-08-19 00:20:26 +0200

    frontend can now handle new playlist api

--- src/components/PlaylistVideo.jsx
+++ src/components/PlaylistVideo.jsx
@@ -3,13 +3,13 @@ import styled from "@emotion/styled";
 
 
 function PlaylistVideo({video}) {
-    const thumbnailsrc = `https://i.ytimg.com/vi/${video}/mqdefault.jpg`
+    const thumbnailsrc = `https://i.ytimg.com/vi/${video.id}/mqdefault.jpg`
     console.log(thumbnailsrc)
 
     return(
         <div>
-            <p>Video title</p>
-            <p>Video Length</p>
+            <p>{video.title}</p>
+            <p>{video.channelTitle}</p>
             <img src={thumbnailsrc} alt='Thumbnail of video'/>
         </div>
     )
