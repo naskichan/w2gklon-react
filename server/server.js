909a219bc91d3369d893e3d96dbeb3432eb95151
Naskichan <gitlab@naski.org>
2021-08-18 14:52:30 +0200

    fixing cross origin errors

--- server/server.js
+++ server/server.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
-
+const cors = require('cors')
+const isDev = process.env.NODE_ENV === "development"
+console.log(isDev)
 const io = require('socket.io')(server)
 let db = require('./data/db.json')
 
 const port = 3001
+
+if(isDev) {
+    app.use(cors());
+}
 
 app.listen(port, function() {
     console.log('backend listening to', port)
