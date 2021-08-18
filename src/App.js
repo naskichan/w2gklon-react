b899f722c4ab36f735ce6cbbd56013d9dcdac328
Naskichan <gitlab@naski.org>
2021-08-19 01:04:39 +0200

    basic design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { useState, React, useEffect } from 'react'
 import axios from 'axios'
 import styled from '@emotion/styled'
-import Playlist from './components/Playlist';
+import Playlist from './components/Playlist'
+import Player from './components/Player'
 
 
 function App() {
 
 
-  let [playlists,setPlayList] = useState([]);
+  let [playlists,setPlayLists] = useState([]);
   
   useEffect( () => {
     async function fetchPlaylists() {
@@ -17,14 +18,17 @@ function App() {
     }
     fetchPlaylists().then((res) => {
       console.log('recieved stuff', res.data)
-      setPlayList(res.data)
+      setPlayLists(res.data)
     })
     // eslint-disable-next-line
   }, [URL]) 
 
   return (
     <Main>
+      <ContentWrapper>
       <Playlist playlists={playlists}/>
+        <Player />
+      </ContentWrapper>
     </Main>
   );
 }
@@ -32,6 +36,11 @@ function App() {
 const Main = styled.div`
   display: flex;
   flex-direction: column;
+`
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-grow: 1 3;
 `
 
 export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+function Player() {
+    return (
+        <Wrapper>
+            <p>Player</p>
+        </Wrapper>
+    )
+}
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+export default Player
