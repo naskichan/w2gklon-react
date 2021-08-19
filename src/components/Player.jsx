3b8ff8e9c8e9264867ff7c4502a0a105e719834e
Naskichan <gitlab@naski.org>
2021-08-19 13:48:18 +0200

    play pause behaviour

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -3,12 +3,34 @@ import styled from "@emotion/styled";
 import YouTube from "react-youtube";
 
 function Player({video}) {
+    // Saving in this file makes the player go undefined
+    let player
+
+    function handleClick() {
+        if(player.getPlayerState() === 1) {
+            player.pauseVideo()
+        } else {
+            player.playVideo()
+        }
+      }
+
+    const playerOpts = {
+        playerVars: {
+            autoplay: 0,
+            controls: 0
+        }
+    }
     return (
         <Wrapper>
-            <YouTube videoId={video.id}></YouTube>
+            <YouTube videoId={video.id} opts={playerOpts} onReady={(e) => player = e.target}></YouTube>
+            <Button onClick={handleClick} />
         </Wrapper>
     )
 }
+
+const Button = styled.button`
+
+`
 
 const Wrapper = styled.div`
     display: flex;
